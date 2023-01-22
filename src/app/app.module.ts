4ecd518271d662a2214a8538c8bb98a54b5f071f
xjanus12x <albertovillacarlos07@gmail.com>
2023-01-22 19:36:32 +0800

    Added About, Education, Skills, Achievements, References components with styles.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { EducationComponent } from './education/education.component';
+import { SkillsComponent } from './skills/skills.component';
+import { AchievementsComponent } from './achievements/achievements.component';
+import { ReferencesComponent } from './references/references.component';
+import { SkillAndReferencesComponent } from './skill-and-references/skill-and-references.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    EducationComponent,
+    SkillsComponent,
+    AchievementsComponent,
+    ReferencesComponent,
+    SkillAndReferencesComponent,
   ],
   imports: [
     BrowserModule,
